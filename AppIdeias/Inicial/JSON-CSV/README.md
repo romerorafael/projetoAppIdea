0029587a85129cee81915f72796613bf471f75be
Rafael Romero <rafaelrromero8@gmail.com>
2020-03-01 10:02:36 -0300

    Modifica Inicial/JSON-CSV/Convertor JSON

--- AppIdeias/Inicial/JSON-CSV/README.md
+++ AppIdeias/Inicial/JSON-CSV/README.md
@@ -13,10 +13,10 @@ App simples que terá o intuito de converter arquivos arquivos CSV em arquivos J
 - [x] O usuário pode ver o texto convertido na caixa de texto.
 
 ## Bônus 1
-- [ ] O usuário pode colar uma sintaxe JSON na caixa de texto.
-- [ ] No momento que o usuário clica no botão CSV o texto é validado e convertido em CSV.
-- [ ] Mensagens de erro são apresentados em caso de não validação.
-- [ ] O usuário pode ver o texto convertido na caixa de texto.
+- [x] O usuário pode colar uma sintaxe JSON na caixa de texto.
+- [x] No momento que o usuário clica no botão CSV o texto é validado e convertido em CSV.
+- [x] Mensagens de erro são apresentados em caso de não validação.
+- [x] O usuário pode ver o texto convertido na caixa de texto.
 
 ## Bônus 2
 - [ ] Usuário pode colar um caminho de CSV/JSON de arquivo no sistema local na caixa de texto.
--- AppIdeias/Inicial/JSON-CSV/recursos/js/script.js
+++ AppIdeias/Inicial/JSON-CSV/recursos/js/script.js
@@ -1,7 +1,13 @@
 $(document).ready(function(){
-    $('.json').click(function(){
+    $('.json').click(()=>{
         if(verificaCSV()){
             CSV2JSON()
+        }
+    })
+
+    $('.csv').click(function(){
+        if(verificaJSON()){
+            JSOn2CSV()
         }
     })
 })
@@ -21,28 +27,47 @@ function verificaCSV(){
     }
     return true
 }
-
-/*Ajuda na identificação do CSV*/
+function verificaJSON(){
+    var texto=$('#original').val()
+    if(texto.length==0){
+        alert('JSON vazio')
+        return false
+    }else if(texto[0]!= '['){
+        alert('Verifique se existe as chaves')
+        return false
+    }
+    return true
+}
+/*Ajuda na identificação do char*/
 function isAlpha(ch){
     return typeof ch === "string" && ch.length === 1 
     && (ch >= "a" && ch <= "z" || ch >= "A" && ch <= "Z");
 }
-
+function isNumber(ch){
+    return typeof ch === "string" && ch.length === 1 
+    && (ch >= "0" && ch <= "9");
+}
 /*Função que transforma o CSV em Json*/
+/*Padronização das palavras, focando nos espaços extras*/
+function padronizaPalavra(palavra){
+    palavra = palavra.trim()
+    return palavra
+}
+/*Converte o texto CSV para JSON*/
 function CSV2JSON(){
 /*Arrays que vão guardar infos importantes*/
-    var json=[]
+    var json= [] //Array que vai guardar o texto final
     var identificadores = [] //Guarda os nomes dos campos
-
+    
 /*Variáveis que recebem informações dos textos*/
-    var texto=$('#original').val().trim()
-    var tam = texto.length
+    var texto=$('#original').val().trim() //Recebe o texto a ser transformado
+    var tam = texto.length //Pega o tamanho do texto que será usado para percorrer
     
     var i=0 //contador para cada char da lista
-
+    
 /*Array que é usada para separar palavras dentro de cada linha*/
-    var palavra=[]
-
+    var palavra= []
+    
 /*Define os identificadores usados no JSON*/
     while(texto[i]!="\n"){
         if(texto[i]!=","){
@@ -56,11 +81,11 @@ function CSV2JSON(){
     identificadores.push(palavra.join(''))
     palavra=[]
     i++
-
-/*Loop que verifica todas as char do texto*/
+    
+    /*Loop que verifica todas as char do texto*/
     while(i<tam){
         var aux=[]
-        /*Separa as palavras da linha*/
+    /*Separa as palavras da linha*/
         while(texto[i]!="\n" && i<tam){
             if(texto[i]!=","){
                 palavra.push(texto[i])  
@@ -73,7 +98,7 @@ function CSV2JSON(){
         aux.push(padronizaPalavra(palavra.join('')))
         palavra=[]
         var aux2 =[]
-        /* Junta para cada elemento do json*/
+    /* Junta para cada elemento do json*/
         aux2.push('{')
         var string=""
         for(var j=0; j<aux.length; j++){
@@ -85,24 +110,72 @@ function CSV2JSON(){
         json.push(aux2.join(''))
         i++
     }  
-
-/*Parte que faz o print no text Area Final*/
-    string='['
-    for(i=0;i<json.length;i++){
-        string+=json[i]
-        if(i!=json.length-1){
-            string+=','
+    
+    /*Parte que faz o print no text Area Final*/
+        string='['
+        for(i=0;i<json.length;i++){
+            string+=json[i]
+            if(i!=json.length-1){
+                string+=','
+            }
+            string+='\n'
         }
-        string+='\n'
-    }
-    string+=']'
-    $('#final').val(string)
-
-    console.log(json)
+        string+=']'
+        $('#final').val(string)
 }
+/*Converte o texto JSON para CSV */
+function JSOn2CSV(){
+    var i=0
+    var primeira = true
 
-function padronizaPalavra(palavra){
-    palavra = palavra.trim()
-    return palavra
+/*Variáveis que recebem informações dos textos*/
+    var texto=$('#original').val().trim() //Recebe o texto a ser transformado
+    var tam = texto.length //Pega o tamanho do texto que será usado para percorrer
+    var string=[]
+
+    console.log(tam)
+/*Loop que verifica todas as char do texto*/
+    while(i<tam){
+        var aux=[]
+        var linha=[]
+    /*Separa linha que será analisado da vez*/
+        while(texto[i]!= '\n' && i<tam){
+            if(isAlpha(texto[i]) || isNumber(texto[i])){
+                aux.push(texto[i])
+            }else if(aux != ''){
+                linha.push(aux.join(''))
+                aux=[]
+            }
+            i++
+        }
+        i++
+    /*Caso seja a primeira passagem ele separa os identificadores*/
+        if(primeira){
+            var indentificador=[]
+            var info=[]
+            for(var j=0; j<linha.length;j++){
+                if(j%2==0){
+                    indentificador.push(linha[j])
+                }else{
+                    info.push(linha[j])
+                }
+            }
+            string+=indentificador+'\n'
+            string+=info+'\n'
+            info=[]
+            primeira=false
+        }else{     /*Nos outros casos ele só verifica a informação*/
+            for(var j=0; j<linha.length;j++){
+                if(j%2!=0){
+                    info.push(linha[j])
+                }
+            }
+            string+=info+'\n'
+            info=[]
+        }
+    /*Monta a string do teexto*/
+        $('#final').val(string)
+    }  
+
+    
 }
-
